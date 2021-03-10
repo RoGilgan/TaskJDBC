4f6a0cd5d21eca14135a4531c32e3efc7d4b28e6
RoGilgan <rogilgan@bk.ru>
2021-03-10 12:28:16 +0200

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,35 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Roman", "Volchek", (byte)24);
+        System.out.println("User с именем Роман добавлен");
+
+        userService.saveUser("Roman", "Volchek", (byte)24);
+        System.out.println("User с именем Роман добавлен");
+
+        userService.saveUser("Roman", "Volchek", (byte)24);
+        System.out.println("User с именем Роман добавлен");
+
+        userService.saveUser("Roman", "Volchek", (byte)24);
+        System.out.println("User с именем Роман добавлен");
+
+        List<User> userList = userService.getAllUsers();
+        userList.forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private final static String HOST = "jdbc:mysql://localhost:3306/jmdb?useUnicode=true&useSSL=true&" +
+            "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "Roman190190Roman";
+
+    public Util() {
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+    }
 }
