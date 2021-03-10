e733c0188486bd35b971a140cfbb65e5ae132e5b
RoGilgan <rogilgan@bk.ru>
2021-03-10 12:20:50 +0200

    Откат

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,5 @@
 package jm.task.core.jdbc.util;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Util {
-
-    private final static String HOST = "jdbc:mysql://localhost:3306/jmdb?useUnicode=true&useSSL=true&" +
-            "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "Roman190190Roman";
-
-    public Util() {
-
-    }
-
-    public static Connection getConnection() throws SQLException {
-
-        return DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-    }
+    // реализуйте настройку соеденения с БД
 }
