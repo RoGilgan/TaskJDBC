f971eb5ecfb9e0ba067aa87bd87b4989cca395e3
RoGilgan <rogilgan@bk.ru>
2021-03-14 13:48:52 +0200

    Initial commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -18,6 +18,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         try (Session session = Util.getSessionFactory().openSession()) {
+
             transaction = session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS user (id BIGINT(19) NOT NULL AUTO_INCREMENT, " +
                     "name VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, " +
